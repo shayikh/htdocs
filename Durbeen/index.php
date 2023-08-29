2b35863052e5af4198cb63588c383205b58f21af
Md Mehrab Alam Shayikh <mshayikh114@gmail.com>
2023-08-29 16:55:50 +0600

    g

--- Durbeen/index.php
+++ Durbeen/index.php
@@ -136,8 +136,9 @@ if (isset($_POST['login'])){
 		</div>
 		<div class="row">
 			<div class="col-md-12">
-				<div style="margin-top: 300px;text-align: center;">
-					<h2 style="color: black">This Website is Designed by <a style="color: blue" href="https://www.facebook.com/tasan.zaman" target="_blank">Ahsan Zaman</a> and Backend Developed by <a style="color: blue" href="https://www.facebook.com/shayikh.fb" target="_blank">Md Mehrab Alam Shayikh</a></h2>
+				<div style="margin-top: 250px;text-align: center;">
+                    <h4 style="color: black">Press <img width="50px" src="./img/ctrl.png" alt=""><img width="40px" src="./img/minus.png" alt=""><img width="40px" src="./img/minus.png" alt=""> to get best design if you use 125% display</h4>
+					<h4 style="color: black">This Website is Designed by <a style="color: blue" href="https://www.facebook.com/tasan.zaman" target="_blank">Ahsan Zaman</a> and Backend Developed by <a style="color: blue" href="https://www.facebook.com/shayikh.fb" target="_blank">Md Mehrab Alam Shayikh</a></h4>
 				</div>
 			</div>
 		</div>
