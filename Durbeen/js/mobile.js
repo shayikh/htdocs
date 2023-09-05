0244f7e96c6f466049abd6c9783e0f1e73155b4f
Md Mehrab Alam Shayikh <mshayikh114@gmail.com>
2023-09-05 22:28:49 +0600

    h

--- Durbeen/js/mobile.js
+++ Durbeen/js/mobile.js
@@ -20,17 +20,6 @@ function showPwd(){
 };
 
 
-window_size()
-function window_size() {
-
-    // let width = window.innerWidth;
-    let width = window.screen.width;
-    if (width < 500){
-        window.location = "./m/"
-    }
-}
-
-
 
 
 
