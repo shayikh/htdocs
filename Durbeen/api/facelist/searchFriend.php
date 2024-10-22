2c3bde70b8822512dc74522acd3e69a8487e215d
Md Mehrab Alam Shayikh <139742573+shayikh@users.noreply.github.com>
2024-10-22 15:32:25 +0600

    o

--- Durbeen/api/facelist/searchFriend.php
+++ Durbeen/api/facelist/searchFriend.php
@@ -8,53 +8,62 @@ $search = mysqli_real_escape_string($connection, $search);
 
 
 
-if (isset($_POST['search'])) {
 
-    $SQL1 = "SELECT * FROM `registration` WHERE name LIKE '$search'";
-    $run1 = mysqli_query($connection, $SQL1);
-    $result = mysqli_num_rows($run1);
-
-    if ($result > 0) {
-        while($data1 = mysqli_fetch_assoc($run1)) {
-            $unique_id_fr = $data1['unique_id'];
+$SQL1 = "SELECT * FROM `registration` ORDER BY `unique_id` DESC";
+$run1 = mysqli_query($connection, $SQL1);
 
 
-            $SQLF = "SELECT * FROM `$unique_id_me follow` WHERE `unique_id_fr`='$unique_id_fr'";
-            $runF = mysqli_query($connection_info,$SQLF);
-            $countF = mysqli_num_rows($runF);
-        
-        
-            ?>
-        
-            <tr>
-                <td class="text-center">
-                    <a href="./people_timeline.php?type&unique_id_fr=<?php echo $unique_id_fr ?>">
-                        <img height="135px" src="../pro_pic/<?php echo $data1['pro_pic'] ?>">
-                    </a>
-                </td>
-                <td class="text-center">
-                    <a class="text-decoration-none" href="./people_timeline.php?type&unique_id_fr=<?php echo $unique_id_fr ?>">
-                        <h3 style="margin-top: 35px"><?php echo $data1['name'] ?></h3>
-                        <h6 class="text-success">Durbeen Visited : <?php echo $data1['visit'] ?></h6>
-                    </a>
-                </td>
-                <td class="text-center">
-                    <button onclick="followfn(<?php echo $unique_id_me ?>, <?php echo $unique_id_fr ?>, this)" class="btn <?php $countF == 0 ? printf("btn-success") : printf("btn-danger") ?>" id="followBtn" style="margin-top: 50px">
-                        <?php $countF == 0 ? printf('<i class="fas fa-user-plus"></i>') : printf('<i class="fas fa-user-slash"></i>') ?>
-                    </button>
-                </td>
-                <td class="text-center">
-                    <a href="message.php?type&unique_id_fr=<?php echo $unique_id_fr?>">
-                        <img width="70px" src="../img/892177.svg" style="margin-top: 35px">
-                    </a>
-                </td>
-            </tr>
-        
-        <?php 
-        }
-    } else {
-        echo 1;
+
+
+while($data1 = mysqli_fetch_assoc($run1)) {
+    $data = $data1['name'];
+    $result = strstr($data, $search);
+
+    if ($result) {
+    
+        $unique_id_fr = $data1['unique_id'];
+
+
+        $SQLF = "SELECT * FROM `$unique_id_me follow` WHERE `unique_id_fr`='$unique_id_fr'";
+        $runF = mysqli_query($connection_info,$SQLF);
+        $countF = mysqli_num_rows($runF);
+
+
+        ?>
+
+        <tr>
+            <td class="text-center">
+                <a href="./people_timeline.php?type&unique_id_fr=<?php echo $unique_id_fr ?>">
+                    <img height="135px" src="../pro_pic/<?php echo $data1['pro_pic'] ?>">
+                </a>
+            </td>
+            <td class="text-center">
+                <a class="text-decoration-none" href="./people_timeline.php?type&unique_id_fr=<?php echo $unique_id_fr ?>">
+                    <h3 style="margin-top: 35px"><?php echo $data1['name'] ?></h3>
+                    <h6 class="text-success">Durbeen Visited : <?php echo $data1['visit'] ?></h6>
+                </a>
+            </td>
+            <td class="text-center">
+                <button onclick="followfn(<?php echo $unique_id_me ?>, <?php echo $unique_id_fr ?>, this)" class="btn <?php $countF == 0 ? printf("btn-success") : printf("btn-danger") ?>" id="followBtn" style="margin-top: 50px">
+                    <?php $countF == 0 ? printf('<i class="fas fa-user-plus"></i>') : printf('<i class="fas fa-user-slash"></i>') ?>
+                </button>
+            </td>
+            <td class="text-center">
+                <a href="message.php?type&unique_id_fr=<?php echo $unique_id_fr?>">
+                    <img width="70px" src="../img/892177.svg" style="margin-top: 35px">
+                </a>
+            </td>
+        </tr>
+
+    <?php 
     }
-
 }
+
+
+
+
+
+
+
+
 
--- Durbeen_2/api/facelist/searchFriend_m.php
+++ Durbeen_2/api/facelist/searchFriend_m.php
@@ -8,61 +8,61 @@ $search = mysqli_real_escape_string($connection, $search);
 
 
 
-if (isset($_POST['search'])) {
 
-    $SQL1 = "SELECT * FROM `registration` WHERE name LIKE '$search'";
-    $run1 = mysqli_query($connection, $SQL1);
-    $result = mysqli_num_rows($run1);
-
-    if ($result > 0) {
-        while($data1 = mysqli_fetch_assoc($run1)) {
-            
-            $unique_id_fr = $data1['unique_id'];
+$SQL1 = "SELECT * FROM `registration` ORDER BY `unique_id` DESC";
+$run1 = mysqli_query($connection, $SQL1);
 
 
-            $SQLF = "SELECT * FROM `$unique_id_me allow` WHERE `unique_id_fr`='$unique_id_fr'";
-            $runF = mysqli_query($connection_info,$SQLF);
-            $countF = mysqli_num_rows($runF);
+
+
+while($data1 = mysqli_fetch_assoc($run1)) {
+    $data = $data1['name'];
+    $result = strstr($data, $search);
+
+    if ($result) {
+    
+        $unique_id_fr = $data1['unique_id'];
+
+
+        $SQLF = "SELECT * FROM `$unique_id_me follow` WHERE `unique_id_fr`='$unique_id_fr'";
+        $runF = mysqli_query($connection_info,$SQLF);
+        $countF = mysqli_num_rows($runF);
+
+
+        ?>
         
-            $SQLC = "SELECT * FROM `$unique_id_me follow` WHERE `unique_id_fr`='$unique_id_fr'";
-            $runC = mysqli_query($connection_info,$SQLC);
-            $countC = mysqli_num_rows($runC);
-        
-        
-            ?>
-        
-            <tr>
-                <td class="text-center">
-                    <a href="./people_timeline.php?type&unique_id_fr=<?php echo $unique_id_fr ?>">
-                        <img style="margin-top: 2px" width="90px" src="../pro_pic/<?php echo $data1['pro_pic'] ?>">
-                    </a>
-                </td>
-                <td class="text-center" style="max-width: 129px">
-                    <a class="text-decoration-none" href="./people_timeline.php?type&unique_id_fr=<?php echo $unique_id_fr ?>">
-                        <p style="font-size: 13px;font-weight: 500"><?php echo $data1['name'] ?></p>
-                        <p class="text-success" style="font-size: 11px;font-weight: 500">Durbeen Visited : <?php echo $data1['visit'] ?></p>
-                    </a>
-                    <button onclick="follow_req(<?php echo $unique_id_me ?>, <?php echo $unique_id_fr ?>, this)" class="btn btn-sm <?php $countC == 0 ? printf('btn-success') : printf("btn-danger") ?>" style="margin-top: 5px">
-                        <?php $countC == 0 ? printf('<i class="fas fa-user-plus"></i>') : printf('<i class="fas fa-user-slash"></i>') ?>
-                    </button>
-                </td>
-                <td class="text-center">
-                    <button onclick="allowfn(<?php echo $unique_id_me ?>, <?php echo $unique_id_fr ?>, this)" class="btn btn-sm <?php $countF == 0 ? printf("btn-success") : printf("btn-danger") ?>" style="margin-top: 2px">
-                        <?php $countF == 0 ? printf('<i class="fas fa-user-check"></i>') : printf('<i class="fas fa-user-times"></i>') ?>
-                    </button>
-                    <br>
-                    <a href="message.php?type&unique_id_fr=<?php echo $unique_id_fr?>">
-                        <img width="40px" src="../img/892177.svg" style="margin-top: 15px">
-                    </a>
-                    
-                </td>
-            </tr>
+        <tr>
+            <td class="text-center">
+                <a href="./people_timeline.php?type&unique_id_fr=<?php echo $unique_id_fr ?>">
+                    <img style="margin-top: 2px" width="90px" src="../pro_pic/<?php echo $data1['pro_pic'] ?>">
+                </a>
+            </td>
+            <td class="text-center" style="max-width: 129px">
+                <a class="text-decoration-none" href="./people_timeline.php?type&unique_id_fr=<?php echo $unique_id_fr ?>">
+                    <p style="font-size: 13px;font-weight: 500"><?php echo $data1['name'] ?></p>
+                    <p class="text-success" style="font-size: 11px;font-weight: 500">Durbeen Visited : <?php echo $data1['visit'] ?></p>
+                </a>
+                <button onclick="follow_req(<?php echo $unique_id_me ?>, <?php echo $unique_id_fr ?>, this)" class="btn btn-sm <?php $countC == 0 ? printf('btn-success') : printf("btn-danger") ?>" style="margin-top: 5px">
+                    <?php $countC == 0 ? printf('<i class="fas fa-user-plus"></i>') : printf('<i class="fas fa-user-slash"></i>') ?>
+                </button>
+            </td>
+            <td class="text-center">
+                <button onclick="allowfn(<?php echo $unique_id_me ?>, <?php echo $unique_id_fr ?>, this)" class="btn btn-sm <?php $countF == 0 ? printf("btn-success") : printf("btn-danger") ?>" style="margin-top: 2px">
+                    <?php $countF == 0 ? printf('<i class="fas fa-user-check"></i>') : printf('<i class="fas fa-user-times"></i>') ?>
+                </button>
+                <br>
+                <a href="message.php?type&unique_id_fr=<?php echo $unique_id_fr?>">
+                    <img width="40px" src="../img/892177.svg" style="margin-top: 15px">
+                </a>
+                
+            </td>
+        </tr>
 
-        <?php 
-        }
-    } else {
-        echo 1;
+
+    <?php 
     }
-
 }
+
+
+
 
