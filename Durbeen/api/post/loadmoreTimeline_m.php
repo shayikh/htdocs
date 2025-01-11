f652b68f33b6ad6a2342c59699870fe6bb6688ab
Md Mehrab Alam Shayikh <139742573+shayikh@users.noreply.github.com>
2025-01-11 14:47:42 +0600

    f

--- Durbeen/api/post/loadmoreTimeline_m.php
+++ Durbeen/api/post/loadmoreTimeline_m.php
@@ -59,7 +59,7 @@ $countdislikeall = mysqli_num_rows($rundislikeall);
             </p>
             <img width="100%" src="../post_image/<?php echo $data1['image']?>">
             <div class="card-body" style="background-color: #198754;border-radius: 0 0 3px 3px">
-                <h6 class="card-title text-white"><?php echo $data1['time']?></h6>
+                <p class="card-title text-white"><?php echo $data1['time']?></p>
                 <p class="card-text text-white"><?php echo $data1['post']?></p>
             </div>
         </div>
